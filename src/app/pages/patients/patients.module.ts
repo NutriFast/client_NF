272a7755f87aa628aa3f7e697b667f7044c331e8
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-09-22 00:02:41 -0300

    Iniciando a contrucao do header para todas as paginas

--- src/app/pages/patients/patients.module.ts
+++ src/app/pages/patients/patients.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PatientsPage } from './patients.page';
-import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from '../../components/explore-container/explore-container.module';
 
 import { PatientsPageRoutingModule } from './patients-routing.module';
 
