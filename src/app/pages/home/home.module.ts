c7d7c20d2165b79922eb70b47966033e7875d38e
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-09-22 21:15:32 -0300

    Iniciando componente de card do paciente

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { ExploreContainerComponentModule } from '../../components/explore-container/explore-container.module';
 
 import { HomePageRoutingModule } from './home-routing.module';
+
+import { ExploreContainerComponentModule } from '../../components/explore-container/explore-container.module';
+import { CardComponentModule } from '../../components/card/card.module';
 
 @NgModule({
   imports: [
@@ -13,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    CardComponentModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
