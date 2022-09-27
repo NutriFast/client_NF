00e084844f97a4c3fcaa6123e6ab01d800765c43
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-09-27 20:59:43 -0300

    Configurando o card para ir para a pagina do paciente

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {}
 
+  goTo(path: string) {
+    this.route.navigate([path]);
+  }
 }
