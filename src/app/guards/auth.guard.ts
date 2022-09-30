384911c699b49ba3ccd6c43033ec77d384c66dfc
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-09-30 00:54:57 -0300

    Adding the guard file

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+
+  constructor(private router: Router) {}
+
+  canLoad() {
+    const isAuthenticated = !!(+localStorage.getItem('authenticated'));
+
+    console.log(isAuthenticated);
+
+    if (isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigateByUrl(
+        this.router.createUrlTree(
+          ['login'], { queryParams:  { redirectTo: this.getRedirectUrl() } }
+        )
+      );
+      return false;
+    }
+  }
+
+  getRedirectUrl() {
+    let url = '/';
+    const navigation = this.router.getCurrentNavigation();
+
+    if (navigation) {
+      url = navigation.extractedUrl.toString();
+    }
+
+    return url;
+  }
+}
