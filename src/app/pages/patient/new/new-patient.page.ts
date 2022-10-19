cfff4865760b32dbfcad23e0ba7607671bb6b41b
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-10-18 22:42:57 -0300

    Implementando o formulário de cadastro de paciente

--- src/app/pages/patient/new/new-patient.page.ts
+++ src/app/pages/patient/new/new-patient.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface Step {
+  id: number;
+  title: string;
+}
 
 @Component({
   selector: 'app-new',
@@ -6,10 +12,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-patient.page.scss'],
 })
 export class NewPatientPage implements OnInit {
+  steps: Array<Step> = [
+    {
+      id: 1,
+      title: 'Qual o nome do seu paciente?',
+    },
+    {
+      id: 2,
+      title: 'Data de nascimento',
+    },
+    {
+      id: 3,
+      title: 'Mais alguns dados',
+    },
+    {
+      id: 4,
+      title: 'Gênero',
+    },
+  ];
+  currentStep: Step;
+  firstStep: Step;
+  lastStep: Step;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe(() => {
+      this.firstStep = this.setFirstStep();
+      this.lastStep = this.setLastStep();
+
+      this.currentStep = this.firstStep;
+    });
   }
 
+  setFirstStep() {
+    return this.steps[0];
+  }
+
+  setLastStep() {
+    return this.steps[this.steps.length - 1];
+  }
+
+  nextStep() {
+    if(!this.checkIsLastStep()) {
+      this.currentStep = this.steps.find(step => step.id === this.currentStep.id + 1);
+    }
+  }
+
+  previousStep() {
+    if(!this.checkIsFirstStep()) {
+      this.currentStep = this.steps.find(step => step.id === this.currentStep.id - 1);
+    }
+  }
+
+  checkIsFirstStep() {
+    return this.currentStep.id === this.firstStep.id;
+  }
+
+  checkIsLastStep() {
+    return this.currentStep.id === this.lastStep.id;
+  }
 }
