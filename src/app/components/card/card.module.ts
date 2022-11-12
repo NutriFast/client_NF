a64c120a5da0646ac0608c91673ac8cdeec88a2b
Lucas Guerra Cavalcante <lucasguerratee@gmail.com>
2022-11-11 23:39:35 -0300

    Removendo espaco desnecessario

--- src/app/components/card/card.module.ts
+++ src/app/components/card/card.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-
 import { CardComponent } from './card.component';
 
 @NgModule({
